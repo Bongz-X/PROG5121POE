e0226f6d2646bbd680a6fccb22a5610bd1da5119
BongaX77 <Bongumusandlela79@gmail.com>
2026-04-11 17:45:30 +0200

    User Registration and validations

--- src/main/java/poe/App.java
+++ src/main/java/poe/App.java
@@ -1,13 +1,46 @@
 package poe;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+    //Scanner to allow user input
+    Scanner input = new Scanner(System.in);
+
+    //Prompt user to enter username
+    System.out.println("Please enter your username (Contains _ and is a max of 5 characers long) ");
+    String username = input.nextLine();
+
+    //Validate username
+    if (username.contains("_") && username.length() <= 5) {
+        System.out.println("Username successfully captured.");
+    } else {
+        System.out.println("Username is not correctly formatted; please ensure that it contains an underscore and is no more than five characters in length.");
     }
+    
+
+    //Prompt user to enter password
+    System.out.println("Please enter your password (8 characters long, contains a capital letter, a number and a special character)");
+    String password = input.nextLine();
+
+    //Validate password
+    if (password.length() <= 8 && password.matches(".*[A-Z].*") && password.matches(".*[0-9].*") && password.matches(".*[!@#$%^&*()].*")) {
+        System.out.println("Password successfully captured.");
+    } else {
+        System.out.println("Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.");
+    }
+
+    //Prompt user to enter SA cell phone no
+    System.out.println("Please enter your SA cell phone number (Must start with +27 and be 12 characters long) ");
+    String cellPhone = input.nextLine();
+
+    //Validate cell phone number
+    if (cellPhone.startsWith("+27") && cellPhone.length() == 12 && cellPhone.matches("\\+27\\d{9}")) {
+        System.out.println("Cell phone number successfully captured.");
+    } else {
+        System.out.println("Cell phone number is not correctly formatted; please ensure that it starts with +27 and is exactly twelve characters long.");
+    }
+  }
 }
